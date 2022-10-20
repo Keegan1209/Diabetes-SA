013ae9e9456293f8a407862f9f8fdbf9467ca442
Keegan <KeeganLFrank@gmail.com>
2022-10-20 22:28:34 +0200

    keegans pushin

---
+++ app/src/main/java/com/example/diabetessa/Reccomendation1.java
@@ -0,0 +1,14 @@
+package com.example.diabetessa;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Reccomendation1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reccomendation);
+    }
+}
---
+++ app/src/main/java/com/example/diabetessa/Tips_and_Tricks.kt
@@ -0,0 +1,11 @@
+package com.example.diabetessa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tips_and_Tricks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tips_and_tricks)
+    }
+}
